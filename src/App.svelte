cf4f042ab22e8da90ad8aba03e2a5874504a9334
jst-r <58705474+jst-r@users.noreply.github.com>
2022-05-18 23:42:48 +0300

    implement top bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
+  import Topbar from "./lib/Topbar.svelte";
 </script>
 
-<main />
+<main>
+  <Topbar />
+  
+</main>
 
 <style>
   :root {
+    --bg-color: hsl(207, 26%, 17%);
+    --elem-bg-color: hsl(209, 23%, 22%);
+    --text-color: hsl(0, 0%, 100%);
+
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    background: var(--bg-color);
+    color: var(--text-color);
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+  :global(body) {
+    margin: 0;
   }
 </style>
---
+++ src/lib/Topbar.svelte
@@ -0,0 +1,19 @@
+<header>
+    <h3>Where in the world?</h3>
+    <span>Dark Mode</span>
+</header>
+
+<style>
+    header {
+        background: var(--elem-bg-color);
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 4em;
+    }
+
+    h3 {
+        line-height: normal;
+    }
+</style>
