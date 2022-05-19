d28c26e92bf6914934d4ca0a2a0b3567d57d757d
jst-r <58705474+jst-r@users.noreply.github.com>
2022-05-19 23:13:05 +0300

    add fonts and content component

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
+  import Content from "./lib/Content.svelte";
   import Topbar from "./lib/Topbar.svelte";
 </script>
 
 <main>
   <Topbar />
-  
+  <Content />
 </main>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap");
+
   :root {
     --bg-color: hsl(207, 26%, 17%);
     --elem-bg-color: hsl(209, 23%, 22%);
     --text-color: hsl(0, 0%, 100%);
 
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    --font-weight-normal: 300;
+    --font-weight-semibold: 600;
+    --font-weight-bold: 800;
+
+    font-family: 'Nunito Sans', sans-serif;
     background: var(--bg-color);
     color: var(--text-color);
+
+    font-weight: var(--font-weight-normal);
   }
 
   :global(body) {
     margin: 0;
+  }
+
+  :global(b) {
+    font-weight: var(--font-weight-bold);
+  }
+
+  :global(.semi-bold) {
+    font-weight: var(--font-weight-semibold);
   }
 </style>
